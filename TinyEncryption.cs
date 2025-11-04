ae8a7c58038811251ac0c36448f78c3cbf05e4f3
Matte Black <matthewbowers147@gmail.com>
2025-11-04 07:56:33 +0000

    yeah

--- TinyEncryption.cs
+++ TinyEncryption.cs
@@ -9,35 +9,35 @@ namespace Prototype_V2
 	class TinyEncryption
 	{
 		uint delta = 0x9e3779b9;
-		int n = 32 ; //the number of rounds to perform#
-		private string Source;
-		private string Hash;
-		private string Key;
+		int n = 32; // the number of rounds to perform
+		private uint[] Source;
+		private uint[] Hash;
+		private uint[] Key;
 
-		public TinyEncryption(string source, string key)
+		public TinyEncryption(string source, uint[] key)
 		{
-			delta = 0x9e3779b9;
-			Source = source;
+			Source = FromString(source);//Convert password string to unsigned integer array
 			Key = key;
-			if (Source.Length % 2 != 0)
-			{
-				Source += " ";
-			}
-			Hash = Encrypt(FromString(Source), Key);
+			//Hash = Encrypt(Source, Key);//Encrypt the password and store the cipertext in Hash
 		}
-		public TinyEncryption(string source, uint deltaOverride)
+		public TinyEncryption(string source, uint deltaOverride, uint[] key)
 		{
 			delta = deltaOverride;
+			Source = FromString(source);
+			Key = key;
+			//Hash = Encrypt(Source, Key);
 		}
 		//v is the data and k is the key
 		//the value for delta is taken from the documentation for tiny encryption algorithm
-		public void Encrypt(uint[] v, uint[] k)
+		public string Encrypt()
 		{
+			uint[] v = Source;
+			uint[] k = Key;
 			uint y = v[0];
 			uint z = v[1];
 			uint sum = 0;
 			//uint delta = 0x9e3779b9;
-			uint n = 32;
+			//uint n = 32;
 
 			while (n-- > 0)
 			{
@@ -48,14 +48,18 @@ namespace Prototype_V2
 
 			v[0] = y;
 			v[1] = z;
+			Hash = v;
+			return $"{y}.{z}";
 		}
-		public void Decrypt(uint[] v, uint[] k)
+		public string Decrypt()
 		{
-			uint n = 32;
+			//uint n = 32;
+			//uint delta = 0x9e3779b9;
+			uint[] v = Hash;
+			uint[] k = Key;
 			uint sum;
 			uint y = v[0];
 			uint z = v[1];
-			uint delta = 0x9e3779b9;
 
 			sum = delta << 5;
 
@@ -68,25 +72,28 @@ namespace Prototype_V2
 
 			v[0] = y;
 			v[1] = z;
+			return ToString(v);
 		}
 		public string ToString(uint[] Unsigned)
 		{
 			string Result = "";
-			for (int i = 0; i < Unsigned.Length; i++)
+			foreach (uint value in Unsigned)
 			{
-				Result = Result + Convert.ToChar(Unsigned[i]);
+				byte[] byteArray = BitConverter.GetBytes(value);
+				foreach (byte b in byteArray)
+				{
+					Result = Result + ((char)b); // Only works for ASCII characters
+				}
 			}
-			return Source;
+			return Result;
+
 		}
 		public uint[] FromString(string Source)
 		{
 			Encoding ascii = Encoding.ASCII;
 			byte[] bytes = ascii.GetBytes(Source);
-
-			// Initialize the array with the correct size
 			uint[] Unsigned = new uint[bytes.Length];
-
-			// Convert each byte to a uint and store it in the array
+			// Convert each byte to a uint
 			for (int i = 0; i < bytes.Length; i++)
 			{
 				Unsigned[i] = Convert.ToUInt32(bytes[i]);
